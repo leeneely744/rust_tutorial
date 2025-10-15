fecc0e5790c5cadc39fdb0b46eb5e430090c0ede
kohei sano <nlee9337@gmail.com>
2025-10-15 22:15:01 +0900

    Add number comparison logic to guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::cmp::Ordering;
 use std::io;
+
 use rand::Rng;
 
 fn main() {
@@ -15,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    
     println!("You gesses: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }

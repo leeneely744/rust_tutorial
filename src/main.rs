9c5dde8ae6c7826ee6f854d80a4921a8c5b3c2e9
kohei sano <nlee9337@gmail.com>
2025-10-19 20:28:41 +0900

    Enhance main function to demonstrate string ownership and cloning with examples

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,13 @@ fn main() {
     println!("------------------");
 
     let s1 = String::from("hello");
+    // This is not shallow copy, this is 'move'
     let s2 = s1;
     // println!("{}", s1);  // error: value borrowed here after move
     println!("{}", s2);
+
+    let ori = String::from("aaa");
+    let col = ori.clone();
+    println!("ori is {}, and col is {}", ori, col);
 
 }

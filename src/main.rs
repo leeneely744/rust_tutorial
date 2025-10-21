6299a5091ac6eae6c3680f85c7dee8c1acc74599
kohei sano <nlee9337@gmail.com>
2025-10-21 22:49:47 +0900

    Add first_word function to main for extracting the first word from a string and update string length calculation demonstration

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,26 @@ fn main() {
     let cl = String::from("hello");
     let len = calculate_length(&cl);
     println!("The length of '{cl} is {len}");
+
+    println!("------------------");
+
+    let s4 = String::from("abcdefg hijklmn");
+    println!("{}", first_word(&s4));
+}
+
+// &str is a slice
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
 
 fn calculate_length(s: &String) -> usize {
+    // s.push_str(", world"); // error: `s` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+                              // `mut` is necessary when declaring s and pass `calculate_length(&mut s)`.
     s.len()
 }

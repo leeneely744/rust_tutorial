a5ef604c0daf28b880bcb33f4c88b1e7f792ad3c
kohei sano <nlee9337@gmail.com>
2025-10-27 22:57:09 +0900

    Update Coin enum to include state variant and modify value_in_cents function to handle state quarters, while commenting out unused print statement in main function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ enum IpAddr {
 
 fn main() {
     let home = IpAddr::V4(String::from("123.1.1.1"));
-    println!("{}", home);
+    // println!("{}", home);
 
     let some_number = Some(5);
     let some_char = Some('a');
@@ -15,20 +15,33 @@ fn main() {
     let x: i8 = 5;
     let y: Option<i8> = Some(5);
     // let sum = x + y; // error: no implementation for `i8 + Option<{integer}>`
+
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // snip
 }
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),  // variant
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
+    // the arms' patterns must cover all possibilities.
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 2,
         Coin::Dime => 3,
-        Coin::Quarter => 4,
+        Coin::Quarter(state) => {
+            println!{"State quarter from {state:?}!"};
+            25
+        }
     }
 }

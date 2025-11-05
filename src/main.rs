adf00f61c60e2542a07fd63153deaa45b0c0fd8d
kohei sano <nlee9337@gmail.com>
2025-11-05 23:42:53 +0900

    Enhance vector manipulation in main function by adding mutable operations and demonstrating borrow checker behavior with immutable references

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
     println!("v is {:?}", v);
+
+    v.push(1);
+    let test = &v[0];
+    println!("test is {test}");
+    
+    v.push(2);
+
+    // This is error: mutable borrow occurs here.
+    // immutable borrow later used here.
+    println!("test is {test}");
+
+    println!("---------------------------");
 
     // vec! is macro
     let mut v2 = vec![10, 20, 30];

aedf5ab50408cad8288eb677f03a53adb3eed26f
kohei sano <nlee9337@gmail.com>
2025-11-05 23:31:22 +0900

    Update vector initialization and mutation in main function, adding error handling for out-of-bounds access and demonstrating Option usage with match statement

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ fn main() {
     println!("v is {:?}", v);
 
     // vec! is macro
-    let mut v2 = vec![1, 2, 3];
+    let mut v2 = vec![10, 20, 30];
     println!("v2 is {:?}", v2);
     // mut is must
-    v2.push(4);
+    v2.push(40);
     println!("v2 is {:?}", v2);
+    
+    let third = v2[2];
+    println!("third is {third}");
+    println!("v2 is {:?}", v2);
+
+    // error
+    // let non = v2[5];
+
+    let number: Option<&i32> = v.get(5);
+    match number {
+        Some(number) => println!("number is {number}"),
+        None => println!("not number"),
+    }
 }
 

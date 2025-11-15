09f261e3b4ea0c26321683ce6cad83f40921f3a0
kohei sano <nlee9337@gmail.com>
2025-11-15 20:11:28 +0900

    Enhance main function by adding string manipulation examples, including concatenation and formatting, while simplifying string initialization for improved clarity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
     // let mut s = String::new();
     let data = "initial contents";
-    let s = data.to_string();
-    let s2 = "init".to_string();
+    // `String::from` is same as `to_string`
+    let mut s = data.to_string();
+    let mut s2 = "init".to_string();
+    // `add` converts &String to &str.
+    let s3 = s + &s2;
+    println!("{}", s3);
+    
+    let mut foo = String::from("foo");
+    let bar = "bar";
+    foo.push_str(bar);
+    println!("{} and {}", foo, bar);
+
+    let a = "tic";
+    let b = "toc";
+    let c = "toe";
+    let abc = format!("{a}-{b}-{c}");
+    println!("{abc}");
 }

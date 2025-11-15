3d75ca655e3709dc3b075ba418bc21fae2edef6d
kohei sano <nlee9337@gmail.com>
2025-11-15 23:15:46 +0900

    Add examples of string handling in main function, including character and byte iteration, to demonstrate Rust's string manipulation capabilities.

--- src/main.rs
+++ src/main.rs
@@ -18,4 +18,17 @@ fn main() {
     let c = "toe";
     let abc = format!("{a}-{b}-{c}");
     println!("{abc}");
+
+    // byte index 4 is not a char boundary; it is inside 'ん' (bytes 3..6) of `こんにちは、ぼくドラえもんです。`
+    // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+    // error: process didn't exit successfully: `target\debug\hello-rust.exe` (exit code: 101)
+    let hello = "こんにちは、ぼくドラえもんです。";
+    // let h = &hello[0..4];
+    // println!("{h}");
+    for c in hello.chars() {  // explicit characters or bytes
+        println!("{c}");
+    }
+    for b in hello.bytes() {
+        println!("{b}");
+    }
 }

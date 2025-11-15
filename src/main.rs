d0f7a7cd07eddf7395ce0f55482b12fb36e2948e
kohei sano <nlee9337@gmail.com>
2025-11-15 19:44:09 +0900

    Refactor main function to simplify string initialization and remove unused vector operations, enhancing code clarity

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,6 @@
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    println!("v is {:?}", v);
-
-    v.push(1);
-    let test = &v[0];
-    println!("test is {test}");
-    
-    v.push(2);
-
-    // This is error: mutable borrow occurs here.
-    // immutable borrow later used here.
-    // println!("test is {test}");
-
-    println!("---------------------------");
-
-    // vec! is macro
-    let mut v2 = vec![10, 20, 30];
-    println!("v2 is {:?}", v2);
-    // mut is must
-    v2.push(40);
-    println!("v2 is {:?}", v2);
-    
-    let third = v2[2];
-    println!("third is {third}");
-    println!("v2 is {:?}", v2);
-
-    // error
-    // let non = v2[5];
-
-    let number: Option<&i32> = v.get(5);
-    match number {
-        Some(number) => println!("number is {number}"),
-        None => println!("not number"),
-    }
-
-    println!("---------------------------");
-
-    let v3 = vec![1,2,3];
-    for i in &v3 {
-        println!("{i}");
-    }
-    let mut v4 = vec![4,5,6];
-    for i in &mut v4 {
-        *i += 10;
-        println!("{i}");
-    }
-
-    println!("---------------------------");
-
-    let row = vec![
-        Cell::Int(3),
-        Cell::Text(String::from("hello")),
-        Cell::Float(10.3),
-    ];
-    println!("Cell is {:?}", row);
-}
-
-// We must be explicit about what types are allowed in this vector.
-#[derive(Debug)]
-enum Cell {
-    Int(i32),
-    Float(f64),
-    Text(String),
+    // let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s2 = "init".to_string();
 }

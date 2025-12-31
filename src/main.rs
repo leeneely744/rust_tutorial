29a4102d70d8c2ed88ae849146bc0f16d25feb29
kohei sano <nlee9337@gmail.com>
2025-12-31 18:50:48 +0900

    Refactor main function to implement a generic Point struct, removing commented-out file handling examples and demonstrating usage of the struct with a simple print statement.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,16 @@
-use std::fs::File;
-// use std::io::ErrorKind;
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
 
 fn main() {
-    // let file_open_result = File::open("hello.txt");  // non exist file.
-    // file_open_result is Result<T,E>
-    // If File::open succeeds, file_open_result is an instance of Ok.
-    // If it fails, an instance of Err.
+    let p = Point { x: 5, y: 10 };
 
-    // let _file = match file_open_result {
-    //     Ok(file) => file,
-    //     Err(error) => panic!("Problem opening the file: {error:?}"),
-    // };
-
-    // this need `use std::io::ErrorKind;`
-    // let _file2 = File::open("hello.txt").unwrap_or_else(|error| {
-    //     if error.kind() == ErrorKind::NotFound {
-    //         File::create("hello.txt").unwrap_or_else(|error| {
-    //             panic!("Problem creating the file: {error:?}");
-    //         })
-    //     } else {
-    //         panic!("Problem opening the file: {error:?}");
-    //     }
-    // });
-
-    // most Rustaceans choose 'expect' rather than 'unwrap'.
-    let _file3 = File::open("hello.txt").expect("my error message");
+    println!("p.x = {}", p.x());
 }

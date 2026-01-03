bcb948bb836351c177eb114370a4679c900fba16
kohei sano <nlee9337@gmail.com>
2026-01-03 19:37:43 +0900

    Add search function to lib.rs and update main.rs to utilize it for displaying search results from file contents.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
+use hello_rust::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,7 +24,9 @@ fn main() {
 
 fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text: \n {contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 

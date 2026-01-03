297a38c1e407822709d9a5df5c46826d2e198a3d
kohei sano <nlee9337@gmail.com>
2026-01-03 17:36:35 +0900

    Add NewsArticle and SocialPost structs with Summary trait implementations, and refactor main function to utilize command line arguments for searching files.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
-}
-
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
-}
+use std::env;
 
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
 
-    let p3 = p1.mixup(p2);
+    let query = &args[1];
+    let file_path = &args[2];
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
